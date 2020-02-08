51aed422406e066db9e8b90a1248332c07833183
skunz42 <skunz598@gmail.com>
2020-02-08 14:33:01 -0500

    added view

--- Driver.py
+++ Driver.py
@@ -1,2 +1,20 @@
 # Sean Kunz
 # Driver
+
+import tkinter as tk
+from View import View
+
+class Driver(tk.Frame):
+    def __init__(self, master=None):
+        #self.database = DB()
+        self.v = View(master)
+        #self.speech = Speech()
+        #self.donate = Donate()
+
+        #self.initialize()
+
+def main():
+    root = tk.Tk()
+    d = Driver(master=root)
+    root.mainloop()
+main()
--- View.py
+++ View.py
@@ -1,2 +1,33 @@
 # Sean Kunz
 # View
+
+import tkinter as tk
+import sys
+
+class View(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.setupWindow()
+
+    def setupWindow(self):
+        # Titles, etc
+        self.master.title('Swear Jar!')
+        self.master.geometry("500x500")
+        self.master.resizable(0,0)
+
+        # Buttons
+        b = tk.Button(self.master, text="Quit", command = lambda : self.quit())
+        b.pack()
+        b.place(x=25, y=10, height=25, width=100)
+
+        b = tk.Button(self.master, text="Donate", command = lambda : self.donate())
+        b.pack()
+        b.place(x=375, y=10, height=25, width=100)
+
+    def quit(self):
+        print(sys.exit())
+
+    def donate(self):
+        print(":^)")
